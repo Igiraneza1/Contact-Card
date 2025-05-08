9427ab7fc939c003aa613d14fb01c8e36c14c71a
Igiraneza1 <igiranezaadeline@gmail.com>
2025-05-08 14:37:21 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// App.tsx or App.jsx
+
+import './App.css';
+
+import ContactCard from './omponents/usercard/UserCard';
+function App() {
+  return (
+    <div className="App">
+      <ContactCard name="Adeline" email="igiranezaadeline@gmail.com" phone="0780000000" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/omponents/usercard/UserCard.tsx
@@ -0,0 +1,19 @@
+import type React from "react";
+
+export interface ContactCardProps {
+  name: string;
+  email: string;
+  phone?: number; 
+}
+
+const ContactCard: React.FC<ContactCardProps> = ({ name, email, phone }) => {
+  return (
+    <div className="contact">
+      <p><strong>Name:</strong> {name}</p>
+      <p><strong>Email:</strong> {email}</p>
+      {phone && <p><strong>Phone:</strong> {phone}</p>}
+    </div>
+  );
+};
+
+export default ContactCard;
